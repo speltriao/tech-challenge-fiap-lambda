46628872f0cad13e006d2b1ff9c5150c8a4b0b96
Alexandre Speltri <alexandrespeltri@gmail.com>
2024-09-28 17:04:13 -0300

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,7 @@
 import json
 import pg8000
 import os
+import jwt
 
 def lambda_handler(event, context):
     token = event['headers'].get('teste')
@@ -9,16 +10,59 @@ def lambda_handler(event, context):
     print(f"Authorization Token: {token}")
     print(f"Method ARN: {method_arn}")
     
-    if token == "SIM":
-        if is_able_to_connect_to_database():
-          return generate_policy('user', 'Allow', method_arn)
-        return generate_policy('user', 'Deny', method_arn)
+    if token:
+        try:
+            cpf_from_token = get_cpf_from_jwt_token(token)
+            if is_valid_customer(cpf_from_token):
+                return generate_policy('user', 'Allow', method_arn)
+            return generate_policy('user', 'Deny', method_arn)
+        except Exception as e:
+            print(f"Error decoding token: {e}")
+            return generate_policy('user', 'Deny', method_arn)
     elif token == "NAO":
         return generate_policy('user', 'Deny', method_arn)
     else:
         raise Exception('Unauthorized!!!!!!!!!!!!')
 
-def generate_policy(principal_id, effect, resource):
+def get_cpf_from_jwt_token(token: str) -> str:
+    try:
+        payload = jwt.decode(token, os.environ['SECRET_KEY'], algorithms=['HS256'])
+        return payload.get('cpf')
+    except jwt.ExpiredSignatureError:
+        raise Exception("Token has expired")
+    except jwt.InvalidTokenError:
+        raise Exception("Invalid token")
+
+def is_valid_customer(cpf_from_token: str) -> bool:
+    db_host = os.environ['DB_HOST']
+    db_name = os.environ['DB_NAME']
+    db_user = os.environ['DB_USER']
+    db_password = os.environ['DB_PASSWORD']
+    
+    query = f"SELECT COUNT(cpf) FROM customer WHERE cpf = '{cpf_from_token}';"
+
+    try:
+        connection = pg8000.connect(
+            host=db_host,
+            database=db_name,
+            user=db_user,
+            password=db_password
+        )
+        cursor = connection.cursor()
+        cursor.execute(query)
+        result = cursor.fetchone()
+        
+        cursor.close()
+        connection.close()
+        
+        return True if result[0] == 1 else False
+
+    except Exception as e:
+        print(f"SQL execution failed: {e}")
+        return False
+
+
+def generate_policy(principal_id, effect, resource) -> dict:
     auth_response = {}
     auth_response['principalId'] = principal_id
     
@@ -36,31 +80,3 @@ def generate_policy(principal_id, effect, resource):
         auth_response['policyDocument'] = policy_document
     
     return auth_response
-  
-
-def is_able_to_connect_to_database():
-    db_host = os.environ['DB_HOST']
-    db_name = os.environ['DB_NAME']
-    db_user = os.environ['DB_USER']
-    db_password = os.environ['DB_PASSWORD']
-    try:
-        connection = pg8000.connect(
-            host=db_host,
-            database=db_name,
-            user=db_user,
-            password=db_password
-        )
-        print(connection)
-        cursor = connection.cursor()
-        print(cursor)
-        cursor.execute("SELECT version();")
-        result = cursor.fetchone()
-        print(f"Database version: {result[0]}")
-        
-        cursor.close()
-        connection.close()
-        
-        return True
-        
-    except Exception as e:
-      return False
